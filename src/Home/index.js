170a9c0d94fbd4d729b6acb86c4c5edc56e9ae2b
komv8i <phani.komaravolu@cchmc.org>
2019-03-12 23:29:57 -0400

    change main page layout

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,16 +1,55 @@
 import React from "react";
-import { Row, Col, Icon } from "antd";
-import MainImage from "../MainImage.png";
+import { Row, Col, Icon, Popover } from "antd";
+import MainImage from "../MainImage.jpg";
 
 function Home() {
   return (
     <div className="main" style={{ textAlign: "center" }}>
       <Row>
-        <Col span={24}>
-          <h1 className="title">Phani Teja Komaravolu</h1>
+        <Col span={11}>
+          <img className="section-image" alt="main" src={MainImage} />
+        </Col>
+        <Col span={1} />
+        <Col span={12}>
+          <Row>
+            <Col span={24}>
+              <h1 className="title">Phani Teja Komaravolu</h1>
+            </Col>
+            <Col span={24}>
+              <h2 className="sub-title">
+                Full Stack Developer & Ardent Lover of ReactJs
+              </h2>
+            </Col>
+            <Row>
+              <Col span={6} />
+              <Col span={6} className="content-footer">
+                <Popover
+                  placement="bottom"
+                  content="Github Profile"
+                  trigger="hover"
+                >
+                  <a href="https://github.com/phaniteja1">
+                    <Icon type="github" />
+                  </a>
+                </Popover>
+              </Col>
+              <Col span={6} className="content-footer">
+                <Popover
+                  placement="bottom"
+                  content="Linkedin Profile"
+                  trigger="hover"
+                >
+                  <a href="https://www.linkedin.com/in/phanitejaone">
+                    <Icon type="linkedin" />
+                  </a>
+                </Popover>
+              </Col>
+              <Col span={6} />
+            </Row>
+          </Row>
         </Col>
       </Row>
-      <Row>
+      {/* <Row>
         <Col span={24}>
           <h2 className="sub-title">
             Full Stack Developer and Ardent Lover of ReactJs
@@ -33,9 +72,9 @@ function Home() {
       </Row>
       <Row>
         <Col span={24}>
-          <img className="section-image" alt="main" src={MainImage} />
+          <img className="section-image" alt="main" src={AnotherImage} />
         </Col>
-      </Row>
+      </Row> */}
     </div>
   );
 }
