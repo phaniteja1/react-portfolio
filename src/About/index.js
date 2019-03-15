924929d5a978e9e850d832a00ceedbe2eb33eaa6
komv8i <phani.komaravolu@cchmc.org>
2019-03-14 23:58:38 -0400

    add links in about me page

--- src/About/index.js
+++ src/About/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col } from "antd";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
@@ -9,8 +10,12 @@ function About() {
           <h1>About Phani Teja Komaravolu</h1>
           <h3 className="about-desc">
             I am a Full Stack Web and Mobile Application Developer currently
-            working for Cincinnati Children's Hospital. I love contributing to
-            open-source development. When I don't code, I do photography!
+            working for Cincinnati Children's Hospital. I{"  "}
+            <span role="img" aria-label="heart">
+              ️❤️
+            </span>{" "}
+            contributing to open-source development. When I don't code, I do
+            <Link to="/photography"> photography!</Link>
           </h3>
           <h3 className="about-desc">
             My current focus in on developing React Hooks-based Packages and an
