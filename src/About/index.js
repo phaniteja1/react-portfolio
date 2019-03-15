47075751df2402cc0154a256b71435f6e0843810
komv8i <phani.komaravolu@cchmc.org>
2019-03-15 00:03:55 -0400

    style emojis

--- src/About/index.js
+++ src/About/index.js
@@ -10,17 +10,20 @@ function About() {
           <h1>About Phani Teja Komaravolu</h1>
           <h3 className="about-desc">
             I am a Full Stack Web and Mobile Application Developer currently
-            working for Cincinnati Children's Hospital. I{"  "}
-            <span role="img" aria-label="heart">
+            working for Cincinnati Children's Hospital. I
+            <span role="img" className="emoji-between-text" aria-label="heart">
               ️❤️
-            </span>{" "}
+            </span>
             contributing to open-source development. When I don't code, I do
             <Link to="/photography"> photography!</Link>
           </h3>
           <h3 className="about-desc">
             My current focus in on developing React Hooks-based Packages and an
             Electron app for workflow management. I actively maintain 5 npm
-            packages and many github projects.
+            packages and many github projects.{" "}
+            <span role="img" className="emoji" aria-label="happy">
+              😀
+            </span>
           </h3>
         </Col>
       </Row>
